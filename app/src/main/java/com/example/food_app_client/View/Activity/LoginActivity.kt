2609644276b6144cb86f09263c582ec18828b9f6
ritaapr <aprilritaapr@gmail.com>
2023-10-24 06:46:00 +0700

    Membuat button dapat berpindah activity di LoginActivity.kt dan RegisterActivity.kt

--- app/src/main/java/com/example/food_app_client/View/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/food_app_client/View/Activity/LoginActivity.kt
@@ -1,12 +1,35 @@
 package com.example.food_app_client.View.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import com.example.food_app_client.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLogin: ImageView = findViewById(R.id.ivLogin)
+        val btnReg: ImageView = findViewById(R.id.ivregister)
+
+        btnLogin.setOnClickListener(this)
+        btnReg.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.ivregister -> {
+                val intentRegkeLog = Intent(this@LoginActivity, RegisterActivity::class.java)
+                startActivity(intentRegkeLog)
+            }
+            R.id.ivLogin -> {
+                val intentKemkeLog = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intentKemkeLog)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/food_app_client/View/Activity/RegisterActivity.kt
+++ app/src/main/java/com/example/food_app_client/View/Activity/RegisterActivity.kt
@@ -1,12 +1,35 @@
 package com.example.food_app_client.View.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.food_app_client.R
+import android.view.View
+import android.widget.ImageView
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val btnRegister: ImageView = findViewById(R.id.ivBtnRegister)
+        val btnKembali: ImageView = findViewById(R.id.ivBtnKembali)
+
+        btnRegister.setOnClickListener(this)
+        btnKembali.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.ivBtnRegister -> {
+                val intentRegkeLog = Intent(this@RegisterActivity, LoginActivity::class.java)
+                startActivity(intentRegkeLog)
+            }
+            R.id.ivBtnKembali -> {
+                val intentKemkeLog = Intent(this@RegisterActivity, LoginActivity::class.java)
+                startActivity(intentKemkeLog)
+            }
+        }
     }
 }
