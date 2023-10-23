3992c210926fe76f567a6a3d7ae2b89f20b6153c
widya0 <widyadhana077@gmail.com>
2023-10-24 06:48:04 +0700

    MenghubungkanLoginActivity dengan RegisterActivity dan UserActivity

--- app/src/main/java/com/example/food_app_client/View/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/food_app_client/View/Activity/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.example.food_app_client.View.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.food_app_client.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val ivRegister = findViewById<ImageView>(R.id.ivregister)
+        val ivLogin = findViewById<ImageView>(R.id.ivLogin)
+
+        ivRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        ivLogin.setOnClickListener {
+            val intent = Intent(this, UserActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
