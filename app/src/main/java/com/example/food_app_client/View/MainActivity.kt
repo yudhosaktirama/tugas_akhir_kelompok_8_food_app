f0ec5991f29fa0b14f2bb8adef0e4ac6db9de68b
yudhosaktirama <yudhosakti89@gmail.com>
2023-10-23 13:31:02 +0700

    Membuat Bottom Navigation pada MainActivity.kt

--- app/src/main/java/com/example/food_app_client/View/MainActivity.kt
+++ app/src/main/java/com/example/food_app_client/View/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.food_app_client.View
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.food_app_client.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
