84329e785a7eab3c6099c7e66b002ac187104c48
yudhosaktirama <yudhosakti89@gmail.com>
2023-11-04 10:32:22 +0700

    Menambahkan viewModel pada fragmentProfile serta observer value yang bersangkutan menggunakan data dari UserViewModel

--- app/src/main/java/com/example/food_app_client/View/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/food_app_client/View/fragment/ProfileFragment.kt
@@ -5,29 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
 import com.example.food_app_client.R
+import com.example.food_app_client.ViewModel.UserViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var tvNama: TextView
+    private lateinit var tvAlamat: TextView
+    private lateinit var tvNoHp : TextView
+    private lateinit var tvNamaTop: TextView
+    private lateinit var tvAlamatTop: TextView
+    private val userViewModel: UserViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -38,23 +32,33 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvNama = view.findViewById(R.id.tvNama)
+        tvAlamat = view.findViewById(R.id.tvAlamatProfile)
+        tvNoHp = view.findViewById(R.id.tvNohp)
+        tvNamaTop = view.findViewById(R.id.tvNamaOrang)
+        tvAlamatTop = view.findViewById(R.id.tvAlamat)
+
+        setInformasi(tvNama,tvAlamat,tvNoHp,tvNamaTop,tvAlamatTop)
+
+
+
+
+    }
+
+    fun setInformasi(tvnama: TextView,tvalamat: TextView,tvnoHp: TextView,tvtopNama: TextView,tvTopAlamat: TextView){
+        userViewModel.nama.observe(viewLifecycleOwner) {newValue ->
+            tvnama.text = newValue
+            tvtopNama.text = newValue
+        }
+        userViewModel.alamat.observe(viewLifecycleOwner){newValue ->
+            tvalamat.text = newValue
+            tvTopAlamat.text = newValue
+        }
+        userViewModel.noHp.observe(viewLifecycleOwner){newValue ->
+            tvnoHp.text = newValue
+        }
+
     }
 }
