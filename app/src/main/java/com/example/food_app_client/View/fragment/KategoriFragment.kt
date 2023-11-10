771027ae8e5aa16758e57d45ea62bfacb36608e2
yudhosaktirama <yudhosakti89@gmail.com>
2023-11-10 19:01:56 +0700

    Membuat Logic KategoriViewModel.kt untuk mengatur list yang akan ditampilkan serta implementasi ke dalam fragment KategoriFragment.kt

--- app/src/main/java/com/example/food_app_client/View/fragment/KategoriFragment.kt
+++ app/src/main/java/com/example/food_app_client/View/fragment/KategoriFragment.kt
@@ -5,10 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.food_app_client.Model.Adapter.AdapterKategori
+import com.example.food_app_client.Model.ListLokal.listdessert
+import com.example.food_app_client.Model.ListLokal.listmakanan
+import com.example.food_app_client.Model.ListLokal.listminuman
 import com.example.food_app_client.R
+import com.example.food_app_client.ViewModel.KategoriViewModel
+import com.example.food_app_client.ViewModel.UserViewModel
 
 
 class KategoriFragment : Fragment() {
+    lateinit var recylerKategori: RecyclerView
+    lateinit var btnKategoriMakanan: ImageView
+    lateinit var btnKategoriMinuman : ImageView
+    lateinit var btnKategoriDessert : ImageView
+    lateinit var textNama: TextView
+    lateinit var textAlamat: TextView
+    val userViewModel : UserViewModel by activityViewModels()
+    val kategoriViewModel : KategoriViewModel by  activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -23,6 +42,54 @@ class KategoriFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        recylerKategori = view.findViewById(R.id.recycleKategori)
+        btnKategoriDessert = view.findViewById(R.id.kategoriDessert)
+        btnKategoriMakanan = view.findViewById(R.id.kategoriMakanan)
+        btnKategoriMinuman = view.findViewById(R.id.kategoriMinuman)
+        textNama = view.findViewById(R.id.tvNamaOrang)
+        textAlamat = view.findViewById(R.id.tvAlamat)
 
+        setNamaAlamat(textNama,textAlamat)
+
+        btnKategoriMinuman.setOnClickListener {
+            setListKategori(listminuman)
+        }
+
+        btnKategoriMakanan.setOnClickListener {
+            setListKategori(listmakanan)
+        }
+
+        btnKategoriDessert.setOnClickListener {
+            setListKategori(listdessert)
+        }
+
+        pantauPerubahanList()
+
+
+
+
+
+
+    }
+
+    fun setNamaAlamat(textNama: TextView, textAlamat: TextView){
+        userViewModel.nama.observe(viewLifecycleOwner){newValue ->
+            textNama.text = newValue
+        }
+
+        userViewModel.alamat.observe(viewLifecycleOwner){newValue ->
+            textAlamat.text = newValue
+        }
+    }
+
+    fun setListKategori(menuBarucuy: List<com.example.food_app_client.Model.ModelClass.Menu>){
+        kategoriViewModel.setMenu(menuBarucuy)
+    }
+
+    fun pantauPerubahanList(){
+        kategoriViewModel.listMakananku.observe(viewLifecycleOwner){newValue ->
+            recylerKategori.adapter = AdapterKategori(newValue)
+            recylerKategori.layoutManager = LinearLayoutManager(requireContext())
+        }
     }
 }
---
+++ app/src/main/java/com/example/food_app_client/ViewModel/KategoriViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.food_app_client.ViewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.food_app_client.Model.ListLokal.listmakanan
+import com.example.food_app_client.Model.ModelClass.Menu
+
+class KategoriViewModel : ViewModel() {
+   private var _listMakanan: MutableLiveData<List<Menu>> = MutableLiveData(listmakanan)
+
+    val listMakananku:LiveData<List<Menu>>
+        get() = _listMakanan
+
+
+    fun setMenu(menuBaru: List<Menu>){
+        _listMakanan.value = menuBaru
+    }
+}
